83b61b975cccd3cadc3ac8c21feba49f81de18a3
ChetanXpro <chetanbaliyan10@gmail.com>
2023-01-07 23:14:51 +0530

    changed complete basic frontend

---
+++ client/src/components/Setting.tsx
@@ -0,0 +1,15 @@
+import { Input } from "@chakra-ui/react";
+import React from "react";
+import { apiInstance } from "../Api/api";
+
+const Setting = () => {
+
+
+  return (
+    <div>
+      <h4>Update your profile pic</h4>
+    </div>
+  );
+};
+
+export default Setting;
