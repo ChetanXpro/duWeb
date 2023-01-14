84180c417b82ae630f5b68dd459e0ddd4a071fef
ChetanXpro <chetanbaliyan10@gmail.com>
2023-01-15 03:50:13 +0530

    api updated

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const API_BASE_URL = "http://localhost:5000";
-const API_BASE_URL = "https://duwebbbb.onrender.com";
+const API_BASE_URL = "https://cloudnotess.azurewebsites.net";
 export const apiInstance = axios.create({
   baseURL: API_BASE_URL,
   timeout: 10000,
