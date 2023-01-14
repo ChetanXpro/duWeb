e37b60ed7f436e354f6247b0443a26b21dae3cb2
ChetanXpro <chetanbaliyan10@gmail.com>
2023-01-15 01:54:16 +0530

    azure fix

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,16 @@ export default defineConfig({
   build: {
     chunkSizeWarningLimit: 1600,
   },
+
+  resolve: {
+    alias: [
+      {
+        find: '@', replacement: path.resolve(__dirname, './src'),
+      },
+      {
+        find: './runtimeConfig', replacement: './runtimeConfig.browser',
+      }
+
+    ]
+  }
 });
