215e867f548b0710d9878be3be0bf82b563ba095
ChetanXpro <chetanbaliyan10@gmail.com>
2023-01-15 23:15:27 +0530

    clean up and minor bug fix

--- src/components/NotesPage/Notes.jsx
+++ src/components/NotesPage/Notes.jsx
@@ -124,9 +124,9 @@ const Notes = ({ name, url, re, id,size }) => {
                       >
                         <path
                           d="M12 15V3m0 12l-4-4m4 4l4-4M2 17l.621 2.485A2 2 0 0 0 4.561 21h14.878a2 2 0 0 0 1.94-1.515L22 17"
-                          stroke-width="2"
-                          stroke-linejoin="round"
-                          stroke-linecap="round"
+                          strokeWidth="2"
+                          strokeLinejoin="round"
+                          strokeLinecap="round"
                           stroke="currentColor"
                           fill="none"
                         ></path>
--- src/main.jsx
+++ src/main.jsx
@@ -2,32 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import {
-  ChakraProvider,
-  ColorModeProvider,
-  Container,
-  ThemeProvider,
-} from "@chakra-ui/react";
+import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import { ReactQueryDevtools } from "react-query/devtools";
+import { BrowserRouter } from "react-router-dom";
 
 export const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <ChakraProvider>
-          {/* <ThemeProvider> */}
-            <ColorModeProvider>
-              <App />
-            </ColorModeProvider>
-          {/* </ThemeProvider> */}
-        </ChakraProvider>
-      </BrowserRouter>
-      {/* <ReactQueryDevtools initialIsOpen /> */}
+      <ChakraProvider>
+        <ColorModeProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </ColorModeProvider>
+      </ChakraProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
